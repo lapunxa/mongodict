ffc693345ab02f3edaffdfd7b5977f9aac2a3017
Eduard Carreras <ecarreras@gisce.net>
2014-01-20 19:13:03 +0100

    Bump to v0.1.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mongodict',
+    version='0.1.0',
+    packages=['mongodict'],
+    url='http://github.com/gisce/mongodict',
+    license='MIT',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    description='Access Mongodb collections as Python dictionaries'
+)
